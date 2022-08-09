b8f2eda5b2c3ee544da5000429fccccf276bc013
terentyev-va <terentyev.va@yandex.ru>
2022-08-09 14:46:56 +0300

    Initial Commit

---
+++ News_SwiftUI/News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_SwiftUIApp.swift
+//  News_SwiftUI
+//
+//  Created by Вячеслав Терентьев on 09.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
